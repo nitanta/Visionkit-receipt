8bcd8da9070adb0876a948e5ab9647f09ac48c68
Nitanta Adhikari <nitanta.adh007@gmail.com>
2022-08-13 09:39:41 +0545

    Initial Commit

---
+++ Receipt_share/Receipt_shareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Receipt_shareApp.swift
+//  Receipt_share
+//
+//  Created by Nitanta Adhikari on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Receipt_shareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
